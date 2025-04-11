f5924b1955048c02c2c2a6734f178312039f7e41
betinakd <AK221509@fi365.ort.edu.uy>
2025-04-11 04:23:18 -0300

    GREEN GetAllClasses_ShouldReturnAllClasses

---
+++ src/Domain/SimClass.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain;
+
+public class SimClass
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? Name { get; set; }
+}
---
+++ src/WebApi/Controllers/SimClassController.cs
@@ -0,0 +1,18 @@
+﻿using IBussinesLogic;
+using Microsoft.AspNetCore.Mvc;
+using Models.Responses;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/v1/classes")]
+public class SimClassController(ISimClassService userService) : ControllerBase
+{
+    private readonly ISimClassService _userService = userService;
+
+    [HttpGet]
+    public IActionResult GetAllSimClass()
+    {
+        return Ok(_userService.GetAllSimClass().Select(x => new SimClassResponse(x)).ToList());
+    }
+}
