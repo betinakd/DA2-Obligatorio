e637b652dbac29ea968c115f0b1b85441a568cd4
betinakd <AK221509@fi365.ort.edu.uy>
2025-04-15 09:45:08 -0300

    GREEN: GetInvocationMethodCorrectly_ShouldThrowOk

---
+++ src/Models/Response/InvocationResponse.cs
@@ -0,0 +1,8 @@
+namespace Models.Response;
+
+public class InvocationResponse()
+{
+    public Guid IdReference { get; set; }
+    public string? MethodName { get; set; }
+    public List<ParameterResponse>? Parametros { get; set; }
+}
