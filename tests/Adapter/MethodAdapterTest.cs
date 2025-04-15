c1760dbcb39a558bb8177ca7063d89bcd0056ccd
Candelaria blanco <cande1911blanco@gmail.com>
2025-04-15 18:42:18 -0300

    [refactor] add local variable correctly

--- tests/Adapter/MethodAdapterTest.cs
+++ tests/Adapter/MethodAdapterTest.cs
@@ -54,4 +54,38 @@ public class MethodAdapterTest
 
         _mockMethodService?.Verify(service => service.AddMethodParameter(methodId, methodRequest.Name, methodRequest.Type), Times.Once);
     }
+
+    [TestMethod]
+    public void AddMethodLocalVariables_ShouldReturnMethodElementsResponse_WhenValidRequest()
+    {
+        var methodId = Guid.NewGuid();
+
+        var relatedClass = new SimClass { Id = Guid.NewGuid(), Name = "ValidClass" };
+        var returnType = new SimAttribute { Id = Guid.NewGuid(), Name = "typeName", RelatedClass = relatedClass };
+        var simMethod = new SimMethod { Id = methodId, Name = "methodName", ReturnType = returnType };
+
+        var methodRequest = new MethodElementsRequest
+        {
+            Name = "name",
+            Type = "typeName"
+        };
+
+        var expectedResponse = new MethodElementsResponse
+        {
+            Id = methodId,
+            Message = "Local variable added successfully"
+        };
+
+        _mockMethodService
+            ?.Setup(service => service.AddLocalVariable(methodId, methodRequest.Name, methodRequest.Type))
+            .Returns(simMethod);
+
+        var result = _methodAdapter?.AddLocalVariable(methodId, methodRequest);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(expectedResponse.Id, result.Id);
+        Assert.AreEqual(expectedResponse.Message, result.Message);
+
+        _mockMethodService?.Verify(service => service.AddLocalVariable(methodId, methodRequest.Name, methodRequest.Type), Times.Once);
+    }
 }
