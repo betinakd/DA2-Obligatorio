72cc0a46671487e1f314f2c2642e8c11bc7d8ba8
max giménez <maxi_nos_10@hotmail.com>
2025-04-17 16:49:16 -0300

    GREEN: CreateAttribute_ShouldThrowException_WhenClassDoesNotExist

--- src/BussinesLogic/SimAttributeService.cs
+++ src/BussinesLogic/SimAttributeService.cs
@@ -1,15 +1,21 @@
+using BussinesLogic.Exceptions;
 using Domain;
 using IBussinesLogic;
 using IDataAccess;
 
 namespace BussinesLogic;
 
-public class SimAttributeService(ISimAttributeDataAccess simAttributeDA) : ISimAttributeService
+public class SimAttributeService(ISimAttributeDataAccess simAttributeDA, ISimClassDataAccess simClassDA) : ISimAttributeService
 {
     private readonly ISimAttributeDataAccess _simAttributeDA = simAttributeDA;
 
     public SimAttribute CreateAttribute(Guid claseId, SimAttribute attribute)
     {
+        if(!simClassDA.ExistSimClassById(claseId))
+        {
+            throw new NonExistentValueLogic("Class does not exist.");
+        }
+
         return _simAttributeDA.CreateAttribute(claseId, attribute);
     }
 
