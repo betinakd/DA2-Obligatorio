6394702b444b4576f82b2b7b951846ddd3561ecc
betinakd <AK221509@fi365.ort.edu.uy>
2025-04-18 13:19:02 -0300

    GREEN: ExecuteMethod_ShouldThrow_WhenInstanceClassNotFound

---
+++ src/BussinesLogic/SimExecutionService.cs
@@ -0,0 +1,21 @@
+using BussinesLogic.Exceptions;
+using Domain;
+using IBussinesLogic;
+using IDataAccess;
+
+namespace BussinesLogic;
+
+public class ExecutionService(ISimClassDataAccess simClassDA) : IExecutionService
+{
+    private readonly ISimClassDataAccess _simClassDA = simClassDA;
+
+    public string ExecuteMethod(string methodName, List<Parameter> parameters, Guid idInstanceType, Guid idReferenceType, string instanceName)
+    {
+        if(!_simClassDA.ExistSimClassById(idInstanceType))
+        {
+            throw new NonExistentValueLogic("Instance class not found.");
+        }
+
+        return null;
+    }
+}
