fb8c053a42b7f99418e054055236667a1a654af9
betinakd <AK221509@fi365.ort.edu.uy>
2025-04-18 13:18:45 -0300

    RED: ExecuteMethod_ShouldThrow_WhenInstanceClassNotFound

---
+++ tests/BussinesLogic/ExecutionServiceTest.cs
@@ -0,0 +1,38 @@
+using BussinesLogic;
+using BussinesLogic.Exceptions;
+using Domain;
+using IDataAccess;
+using Moq;
+
+namespace Tests.BussinesLogic;
+
+[TestClass]
+public class ExecutionServiceTest
+{
+    private Mock<ISimClassDataAccess>? _mockSimClassDataAccess;
+    private ExecutionService? _executionService;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _mockSimClassDataAccess = new Mock<ISimClassDataAccess>(MockBehavior.Strict);
+        _executionService = new ExecutionService(_mockSimClassDataAccess.Object);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(NonExistentValueLogic))]
+    public void ExecuteMethod_ShouldThrow_WhenInstanceClassNotFound()
+    {
+        var methodName = "TestMethod";
+        var parameters = new List<Parameter>();
+        var idInstanceType = Guid.NewGuid();
+        var idReferenceType = Guid.NewGuid();
+        var instanceName = "TestInstance";
+
+        _mockSimClassDataAccess!
+            .Setup(m => m.ExistSimClassById(idInstanceType))
+            .Returns(false);
+
+        _executionService!.ExecuteMethod(methodName, parameters, idInstanceType, idReferenceType, instanceName);
+    }
+}
