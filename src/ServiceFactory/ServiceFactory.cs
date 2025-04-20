c81ddaf26c33842ee42b0a808b236632b014f1da
betinakd <AK221509@fi365.ort.edu.uy>
2025-04-20 15:37:29 -0300

    Add: Service Factory

---
+++ src/ServiceFactory/ServiceFactory.cs
@@ -0,0 +1,32 @@
+﻿using BussinesLogic;
+using DataAccess.Context;
+using IAdapter;
+using IBussinesLogic;
+using IDataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceFactory;
+
+public static class ServiceFactory
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<ISimClassAdapter, SimClassAdapter>();
+        services.AddScoped<ISimClassService, SimClassService>();
+        services.AddScoped<ISimClassDataAccess, SimClassDataAccess>();
+        services.AddScoped<IMethodAdapter, MethodAdapter>();
+        services.AddScoped<IMethodService, MethodService>();
+        services.AddScoped<ISimMethodDataAccess, MethodDataAccess>();
+        services.AddScoped<IAttributeAdapter, AttributeAdapter>();
+        services.AddScoped<ISimAttributeService, SimAttributeService>();
+        services.AddScoped<ISimAttributeDataAccess, SimAttributeDataAccess>();
+        services.AddScoped<IExecutionAdapter, ExecutionAdapter>();
+        services.AddScoped<IExecutionService, ExecutionService>();
+        services.AddScoped<IExecutionDataAccess, ExecutionDataAccess>();
+        services.AddDbContext<SimulatorDbContext>(options =>
+            options.UseSqlServer("DefaultConnection"));
+
+        return services;
+    }
+}
