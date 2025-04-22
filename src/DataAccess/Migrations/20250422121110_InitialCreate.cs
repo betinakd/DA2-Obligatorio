ae4a230c557c1f6d7d2cce13adc14d682eefba33
betinakd <AK221509@fi365.ort.edu.uy>
2025-04-22 10:41:01 -0300

    GREEN: docker compose for database

--- src/DataAccess/Migrations/20250422121110_InitialCreate.cs
+++ src/DataAccess/Migrations/20250422121110_InitialCreate.cs
@@ -125,13 +125,13 @@ namespace DataAccess.Migrations
                         column: x => x.TypeId,
                         principalTable: "SimClasses",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
+                        onDelete: ReferentialAction.Restrict);
                     table.ForeignKey(
                         name: "FK_LocalVariables_SimMethods_RelatedMethodId",
                         column: x => x.RelatedMethodId,
                         principalTable: "SimMethods",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
+                        onDelete: ReferentialAction.Restrict);
                 });
 
             migrationBuilder.CreateTable(
@@ -171,20 +171,20 @@ namespace DataAccess.Migrations
                 columns: new[] { "Id", "BaseClassId", "Name", "State" },
                 values: new object[,]
                 {
-                    { new Guid("6c7032af-930b-4f6a-b69b-f9649e46184b"), null, "Object", 2 },
-                    { new Guid("2112163b-6700-4901-926a-438d34eab0d1"), new Guid("6c7032af-930b-4f6a-b69b-f9649e46184b"), "int", 2 },
-                    { new Guid("24c8422d-17b6-490a-b10e-0eca9b1bcf03"), new Guid("6c7032af-930b-4f6a-b69b-f9649e46184b"), "float", 2 },
-                    { new Guid("465cf4a9-137b-4517-951d-2e61e2f44676"), new Guid("6c7032af-930b-4f6a-b69b-f9649e46184b"), "decimal", 2 },
-                    { new Guid("5b57e7d7-44ac-40ba-9d27-4763e42f4cf4"), new Guid("6c7032af-930b-4f6a-b69b-f9649e46184b"), "char", 2 },
-                    { new Guid("68e07265-d232-4569-b6eb-34172c0a0f56"), new Guid("6c7032af-930b-4f6a-b69b-f9649e46184b"), "double", 2 },
-                    { new Guid("7036f7bf-dd57-4626-9520-fcc01965b645"), new Guid("6c7032af-930b-4f6a-b69b-f9649e46184b"), "string", 2 },
-                    { new Guid("b71b8f93-2d44-4a71-af57-35d3a756a02f"), new Guid("6c7032af-930b-4f6a-b69b-f9649e46184b"), "bool", 2 }
+                    { new Guid("658ec69e-54b6-4a7c-a7ab-02ca4ee04145"), null, "Object", 2 },
+                    { new Guid("12c595ed-ba10-4036-86a2-00799e269828"), new Guid("658ec69e-54b6-4a7c-a7ab-02ca4ee04145"), "char", 2 },
+                    { new Guid("35028755-5c75-4528-b9d1-0fe92c8a1abc"), new Guid("658ec69e-54b6-4a7c-a7ab-02ca4ee04145"), "bool", 2 },
+                    { new Guid("54502f3b-08d6-4cc9-95b0-93c363e12c76"), new Guid("658ec69e-54b6-4a7c-a7ab-02ca4ee04145"), "int", 2 },
+                    { new Guid("a4a4d609-1744-4e48-a60b-b7a20d457d8f"), new Guid("658ec69e-54b6-4a7c-a7ab-02ca4ee04145"), "decimal", 2 },
+                    { new Guid("b115d7fd-4dc0-488f-a0ca-226b050030a3"), new Guid("658ec69e-54b6-4a7c-a7ab-02ca4ee04145"), "string", 2 },
+                    { new Guid("c5aabc97-b42f-45f6-bbb8-f4ace28dab73"), new Guid("658ec69e-54b6-4a7c-a7ab-02ca4ee04145"), "float", 2 },
+                    { new Guid("dc7bfbfa-781c-4778-a753-64176190adb1"), new Guid("658ec69e-54b6-4a7c-a7ab-02ca4ee04145"), "double", 2 }
                 });
 
             migrationBuilder.InsertData(
                 table: "SimMethods",
                 columns: new[] { "Id", "Accesibility", "Name", "Privacity", "RelatedClassId", "ReturTypeId" },
-                values: new object[] { new Guid("eb8e9d45-3f1f-446f-a4b5-1657cc3f209f"), 2, "Equals", 0, new Guid("6c7032af-930b-4f6a-b69b-f9649e46184b"), new Guid("b71b8f93-2d44-4a71-af57-35d3a756a02f") });
+                values: new object[] { new Guid("5f3e1e52-4e4a-465e-80df-ba6b1c6b2bbd"), 2, "Equals", 2, new Guid("658ec69e-54b6-4a7c-a7ab-02ca4ee04145"), new Guid("35028755-5c75-4528-b9d1-0fe92c8a1abc") });
 
             migrationBuilder.CreateIndex(
                 name: "IX_Invocations_RelatedMethodId",
