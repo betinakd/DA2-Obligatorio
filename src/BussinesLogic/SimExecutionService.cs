d6f31f10945c6bc44b4feaca29f3043d486fbca2
betinakd <AK221509@fi365.ort.edu.uy>
2025-04-27 01:27:13 -0300

    GREEN: ExecuteMethod_BasicMethodWithoutInvocations_ReturnsFormattedOutput

--- src/BussinesLogic/SimExecutionService.cs
+++ src/BussinesLogic/SimExecutionService.cs
@@ -1,47 +1,22 @@
-using BussinesLogic.Exceptions;
 using Domain;
 using IBussinesLogic;
 using IDataAccess;
 
 namespace BussinesLogic;
 
-public class ExecutionService(ISimClassDataAccess simClassDA, IExecutionDataAccess executionDataAccess) : IExecutionService
+public class ExecutionService(IExecutionDataAccess executionDataAccess) : IExecutionService
 {
-    private readonly ISimClassDataAccess _simClassDA = simClassDA;
     private readonly IExecutionDataAccess _executionDA = executionDataAccess;
 
-    public string ExecuteMethod(string methodName, List<Parameter> parameters, Guid idInstanceType, Guid idReferenceType, string instanceName)
+    public string ExecuteMethod(Reference reference, Reference objReal, Signature signature, int level = 0, HashSet<Guid>? visited = null)
     {
-        if(!_simClassDA.ExistSimClassById(idInstanceType))
-        {
-            throw new NonExistentValueLogic("Instance class not found.");
-        }
+        var identation = new string(' ', level * 4);
 
-        if(!_simClassDA.ExistSimClassById(idReferenceType))
-        {
-            throw new NonExistentValueLogic("Reference class not found.");
-        }
+        SimClass objClass = objReal.GetSimClass();
+        SimMethod? methodToExecute = _executionDA.FindMethodInHierarchy(objClass, signature);
 
-        if(_executionDA.NotFoundMethodFirm(methodName, parameters, idInstanceType, idReferenceType))
-        {
-            throw new NonExistentValueLogic("Method not found in class or its base classes.");
-        }
+        var result = $"{identation}{reference.GetSignature(signature)} -> {methodToExecute.RelatedClass.Name}.{methodToExecute.Name}()";
 
-        if(_executionDA.ExecuteAbstractMethod(methodName, parameters, idInstanceType, idReferenceType))
-        {
-            throw new InvalidOperationLogic($"Can not execute {methodName} because it is abstract.");
-        }
-
-        if(_executionDA.FoundSealedMethod(methodName, parameters, idInstanceType, idReferenceType))
-        {
-            throw new InvalidOperationLogic($"Can not execute {methodName} because it is sealed for the instance.");
-        }
-
-        if(_executionDA.FoundPrivateMethod(methodName, parameters, idInstanceType, idReferenceType))
-        {
-            throw new InvalidOperationLogic($"Can not execute a private method.");
-        }
-
-        return _executionDA.GetExecution(methodName, parameters, idInstanceType, idReferenceType);
+        return result;
     }
 }
