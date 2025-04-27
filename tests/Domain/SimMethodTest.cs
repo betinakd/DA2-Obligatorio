2089400b42a82e51dec7f6629816e2c78ecabbf1
betinakd <AK221509@fi365.ort.edu.uy>
2025-04-26 22:09:50 -0300

    GREEN: MatchSignature method

---
+++ tests/Domain/SimMethodTest.cs
@@ -0,0 +1,159 @@
+using Domain;
+
+namespace Tests.Domain;
+
+[TestClass]
+public class SimMethodTest
+{
+    [TestMethod]
+    public void MatchSignature_WithMatchingSignature_ReturnsTrue()
+    {
+        var intType = new SimClass { Id = Guid.NewGuid(), Name = "int" };
+        var stringType = new SimClass { Id = Guid.NewGuid(), Name = "string" };
+
+        var method = new SimMethod
+        {
+            Name = "TestMethod",
+            Parameters =
+            [
+                new Parameter { Name = "param1", TypeId = intType.Id, Type = intType },
+                new Parameter { Name = "param2", TypeId = stringType.Id, Type = stringType }
+            ]
+        };
+
+        var signature = new Signature
+        {
+            Name = "TestMethod",
+            Parameters =
+            [
+                new ParameterSignature { Name = "x", TypeId = intType.Id },
+                new ParameterSignature { Name = "y", TypeId = stringType.Id }
+            ]
+        };
+
+        var result = method.MatchSignature(signature);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void MatchSignature_WithDifferentName_ReturnsFalse()
+    {
+        var intType = new SimClass { Id = Guid.NewGuid(), Name = "int" };
+
+        var method = new SimMethod
+        {
+            Name = "TestMethod",
+            Parameters =
+            [
+                new Parameter { Name = "param1", TypeId = intType.Id, Type = intType }
+            ]
+        };
+
+        var signature = new Signature
+        {
+            Name = "DifferentMethod",
+            Parameters =
+            [
+                new ParameterSignature { Name = "x", TypeId = intType.Id }
+            ]
+        };
+
+        var result = method.MatchSignature(signature);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void MatchSignature_WithDifferentParameterCount_ReturnsFalse()
+    {
+        var intType = new SimClass { Id = Guid.NewGuid(), Name = "int" };
+        var stringType = new SimClass { Id = Guid.NewGuid(), Name = "string" };
+
+        var method = new SimMethod
+        {
+            Name = "TestMethod",
+            Parameters =
+            [
+                new Parameter { Name = "param1", TypeId = intType.Id, Type = intType },
+                new Parameter { Name = "param2", TypeId = stringType.Id, Type = stringType }
+            ]
+        };
+
+        var signature = new Signature
+        {
+            Name = "TestMethod",
+            Parameters =
+            [
+                new ParameterSignature { Name = "x", TypeId = intType.Id }
+            ]
+        };
+
+        var result = method.MatchSignature(signature);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void MatchSignature_WithDifferentParameterTypes_ReturnsFalse()
+    {
+        var intType = new SimClass { Id = Guid.NewGuid(), Name = "int" };
+        var stringType = new SimClass { Id = Guid.NewGuid(), Name = "string" };
+        var doubleType = new SimClass { Id = Guid.NewGuid(), Name = "double" };
+
+        var method = new SimMethod
+        {
+            Name = "TestMethod",
+            Parameters =
+            [
+                new Parameter { Name = "param1", TypeId = intType.Id, Type = intType },
+                new Parameter { Name = "param2", TypeId = stringType.Id, Type = stringType }
+            ]
+        };
+
+        var signature = new Signature
+        {
+            Name = "TestMethod",
+            Parameters =
+            [
+                new ParameterSignature { Name = "x", TypeId = intType.Id },
+                new ParameterSignature { Name = "y", TypeId = doubleType.Id } // Different type
+            ]
+        };
+
+        var result = method.MatchSignature(signature);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void MatchSignature_WithDifferentParameterOrder_ReturnsFalse()
+    {
+        var intType = new SimClass { Id = Guid.NewGuid(), Name = "int" };
+        var stringType = new SimClass { Id = Guid.NewGuid(), Name = "string" };
+
+        var method = new SimMethod
+        {
+            Name = "TestMethod",
+            Parameters =
+            [
+                new Parameter { Name = "param1", TypeId = intType.Id, Type = intType },
+                new Parameter { Name = "param2", TypeId = stringType.Id, Type = stringType }
+            ]
+        };
+
+        var signature = new Signature
+        {
+            Name = "TestMethod",
+            Parameters =
+            [
+                new ParameterSignature { Name = "y", TypeId = stringType.Id }, // Swapped order
+                new ParameterSignature { Name = "x", TypeId = intType.Id }
+            ]
+        };
+
+        var result = method.MatchSignature(signature);
+
+        Assert.IsFalse(result);
+    }
+}
