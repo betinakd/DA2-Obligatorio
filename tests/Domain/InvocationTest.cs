e07bdceb65fd3890e3e133a665263007bd6db420
max giménez <maxi_nos_10@hotmail.com>
2025-04-28 00:42:21 -0300

    RED: SetReference_ToNull_ThrowsSimClassInvalidAttribute

---
+++ tests/Domain/InvocationTest.cs
@@ -0,0 +1,30 @@
+using Domain;
+using Domain.Exceptions;
+
+namespace Tests.Domain;
+
+[TestClass]
+public class InvocationTest
+{
+    private Invocation? invocation;
+    private Reference? validReference;
+    private Signature? validSignature;
+    private Guid validMethodId;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        invocation = new Invocation();
+        validReference = new ReferenceThis();
+        validSignature = new Signature { Name = "TestMethod" };
+        validMethodId = Guid.NewGuid();
+    }
+
+    // Reference property tests
+    [TestMethod]
+    [ExpectedException(typeof(SimClassInvalidAttribute))]
+    public void SetReference_ToNull_ThrowsSimClassInvalidAttribute()
+    {
+        invocation.Reference = null;
+    }
+}
