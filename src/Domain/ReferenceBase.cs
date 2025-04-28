34893c14718fa31b3f74103d1b5e024d080aa8e8
betinakd <AK221509@fi365.ort.edu.uy>
2025-04-27 22:20:52 -0300

    GREEN: TestGetSignatureWithClassNameBase_ShouldReturnCorrectFormat

--- src/Domain/ReferenceBase.cs
+++ src/Domain/ReferenceBase.cs
@@ -14,7 +14,8 @@ public class ReferenceBase : Reference
 
     public override string GetSignatureWithClassName(Signature signature)
     {
-        throw new NotImplementedException();
+        var simParams = string.Join(", ", signature.Parameters.Select(p => p.Name));
+        return Reference.BaseClass.Name + "." + signature.Name + "(" + simParams + ")";
     }
 
     public override SimClass GetSimClass()
--- tests/Domain/ReferenceBaseTest.cs
+++ tests/Domain/ReferenceBaseTest.cs
@@ -50,4 +50,25 @@ public class ReferenceBaseTest()
 
         referenceBase.GetSimClass();
     }
+
+    [TestMethod]
+    public void TestGetSignatureWithClassNameBase_ShouldReturnCorrectFormat()
+    {
+        var signature = new Signature
+        {
+            Name = "MyMethod",
+            Parameters =
+            [
+                new ParameterSignature { Name = "x" },
+            new ParameterSignature { Name = "y" }
+            ]
+        };
+        var baseClass = new SimClass { Name = "BaseClass" };
+        var simClass = new SimClass { Name = "ChildClass", BaseClass = baseClass, BaseClassId = baseClass.Id };
+        var referenceBase = new ReferenceBase { Reference = simClass };
+
+        var result = referenceBase.GetSignatureWithClassName(signature);
+
+        Assert.AreEqual("BaseClass.MyMethod(x, y)", result);
+    }
 }
