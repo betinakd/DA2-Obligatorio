a27aca7ac5df1839956866319abce6c639c94fc1
max giménez <maxi_nos_10@hotmail.com>
2025-04-28 00:45:49 -0300

    GREEN: SetReference_ToNull_ThrowsSimClassInvalidAttribute

--- src/Domain/Invocation.cs
+++ src/Domain/Invocation.cs
@@ -1,9 +1,24 @@
+using Domain.Exceptions;
+
 namespace Domain;
 public class Invocation()
 {
     public Guid Id { get; set; } = Guid.NewGuid();
 
-    public Reference Reference { get; set; } = null;
+    private Reference? _reference;
+    public Reference Reference
+    {
+        get => _reference;
+        set
+        {
+            if(value == null)
+            {
+                throw new SimClassInvalidAttribute("Reference cannot be null.");
+            }
+
+            _reference = null;
+        }
+    }
 
     public Guid? ReferenceId { get; set; }
 
