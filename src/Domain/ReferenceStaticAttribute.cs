d0db037b28e29f0060532a61e37af3ccce31ceef
betinakd <AK221509@fi365.ort.edu.uy>
2025-05-26 19:08:56 -0300

    GREEN: ReferenceStaticAttribute

---
+++ src/Domain/ReferenceStaticAttribute.cs
@@ -0,0 +1,55 @@
+using Domain.Exceptions;
+
+namespace Domain;
+
+public class ReferenceStaticAttribute : Reference
+{
+    private SimAttribute _reference = new SimAttribute();
+    public SimAttribute Reference
+    {
+        get => _reference;
+        set
+        {
+            if(value != null && !value.IsStatic)
+            {
+                throw new InvalidAttributeDomain("Only static attributes allowed on ReferenceStaticAttribute.");
+            }
+
+            _reference = value;
+        }
+    }
+
+    public Guid ReferenceId { get; set; }
+
+    public override string GetSignature(Signature method)
+    {
+        var simParams = string.Join(", ", method.Parameters.Select(p => p.Name));
+        return Reference.Name + "." + method.Name + "(" + simParams + ")";
+    }
+
+    public override string GetSignatureWithClassName(Signature signature)
+    {
+        var simParams = string.Join(", ", signature.Parameters.Select(p => p.Name));
+        return Reference.Type.Name + "." + signature.Name + "(" + simParams + ")";
+    }
+
+    public override SimClass GetSimClass()
+    {
+        if(Reference.Type == null)
+        {
+            throw new InvalidAttributeDomain("Reference type cannot be null.");
+        }
+
+        return Reference.Type;
+    }
+
+    public override Guid GetReferenceId()
+    {
+        return Reference.Id;
+    }
+
+    public override string GetReferenceTypeDescription()
+    {
+        return "Attribute";
+    }
+}
