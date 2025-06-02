f6abcd384ec9738499d6dbf6c6ac7cb25db0792f
betinakd <AK221509@fi365.ort.edu.uy>
2025-06-01 21:24:48 -0300

    add: pages and components

--- frontend/src/app/pages/classes/create/create.component.ts
+++ frontend/src/app/pages/classes/create/create.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormClassComponent } from '../../../components/form-create-class/form-class.component';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [CommonModule, FormClassComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
