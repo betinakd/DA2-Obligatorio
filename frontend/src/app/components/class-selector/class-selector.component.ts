04154be8d1b4fbc61aa21d4be4fe5592072dd1c3
betinakd <AK221509@fi365.ort.edu.uy>
2025-06-03 01:33:08 -0300

    add: class selector

---
+++ frontend/src/app/components/class-selector/class-selector.component.ts
@@ -0,0 +1,59 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { ClassService } from '../../services/class.service';
+import { SimClass } from '../../models/SimClass.model';
+
+@Component({
+  selector: 'app-class-selector',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatProgressSpinnerModule
+  ],
+  templateUrl: './class-selector.component.html',
+  styleUrls: ['./class-selector.component.scss']
+})
+export class ClassSelectorComponent implements OnInit {
+  @Output() classSelected = new EventEmitter<string>();
+  @Input() labelText = 'Seleccionar Clase';
+
+  classControl = new FormControl('');
+  classes: SimClass[] = [];
+  loading = false;
+  error: string | null = null;
+  
+  constructor(private classService: ClassService) {}
+  
+  ngOnInit(): void {
+    this.loadClasses();
+    
+    this.classControl.valueChanges.subscribe(value => {
+      this.classSelected.emit(value || '');
+    });
+  }
+  
+  loadClasses(): void {
+    this.loading = true;
+    this.error = null;
+    
+    this.classService.getAllClasses().subscribe({
+      next: (data) => {
+        this.classes = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.loading = false;
+        this.error = 'Error al cargar clases';
+        console.error('Error cargando clases:', err);
+      }
+    });
+  }
+}
