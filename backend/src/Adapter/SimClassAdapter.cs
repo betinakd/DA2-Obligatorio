dcbedae08cdcc42e706710cb8a9a1bd208e3062b
betinakd <AK221509@fi365.ort.edu.uy>
2025-06-06 02:57:07 -0300

    REFACTOR: Remove add interface

--- backend/src/Adapter/SimClassAdapter.cs
+++ backend/src/Adapter/SimClassAdapter.cs
@@ -193,29 +193,4 @@ public class SimClassAdapter(ISimClassService simClassService, IMethodService me
             throw new NonExistentValueAdapter(ex.Message);
         }
     }
-
-    public CreatedSimClassResponse AddInterface(Guid id, InterfaceRequestUpdate methodRequest)
-    {
-        try
-        {
-            var simClass = _simClassService.AddInterface(id, methodRequest.InterfaceId);
-            return new CreatedSimClassResponse() { Message = "Interface implemented successfully.", SimClass = SimClassResponseMapper.MapToSimClassResponse(simClass) };
-        }
-        catch(NonExistentValueLogic ex)
-        {
-            throw new NonExistentValueAdapter(ex.Message);
-        }
-        catch(InUseValueLogic ex)
-        {
-            throw new InUseValueAdapter(ex.Message);
-        }
-        catch(InvalidAttributeLogic ex)
-        {
-            throw new InvalidAttributeAdapter(ex.Message);
-        }
-        catch(InvalidAttributeDomain ex)
-        {
-            throw new InvalidAttributeAdapter(ex.Message);
-        }
-    }
 }
--- backend/src/IAdapter/ISimClassAdapter.cs
+++ backend/src/IAdapter/ISimClassAdapter.cs
@@ -10,5 +10,4 @@ public interface ISimClassAdapter
     UpdateSimClassResponse UpdateSimClass(SimClassRequestUpdate request, Guid idSimClass);
     void DeleteSimClass(Guid id);
     SimClassResponse GetSimClassInfo(Guid classId);
-    CreatedSimClassResponse AddInterface(Guid id, InterfaceRequestUpdate methodRequest);
 }
