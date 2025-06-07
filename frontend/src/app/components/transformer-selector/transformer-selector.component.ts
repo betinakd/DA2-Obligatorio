5a17aa15937fa20ce4e8560c264191f7616848af
betinakd <AK221509@fi365.ort.edu.uy>
2025-06-07 18:42:45 -0300

    add: selector transformers types

--- frontend/src/app/components/transformer-selector/transformer-selector.component.ts
+++ frontend/src/app/components/transformer-selector/transformer-selector.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { TransformersService } from '../../services/transformers.service';
 
 @Component({
   selector: 'app-transformer-selector',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
   templateUrl: './transformer-selector.component.html',
   styleUrl: './transformer-selector.component.scss'
 })
-export class TransformerSelectorComponent {
+export class TransformerSelectorComponent implements OnInit {
+  @Output() transformerSelected = new EventEmitter<string>();
+  @Input() labelText = 'Select Transformer';
 
+  transformerControl = new FormControl('');
+  transformers: string[] = [];
+
+  constructor(private transformersService: TransformersService) { }
+
+  ngOnInit(): void {
+    this.transformersService.getTransformers().subscribe({
+      next: (data) => {
+        this.transformers = data;
+
+        if (this.transformers.length > 0) {
+          this.transformerControl.setValue(this.transformers[0]);
+          this.transformerSelected.emit(this.transformers[0]);
+        }
+      },
+      error: (error) => console.error('Error loading transformers:', error)
+    });
+
+    this.transformerControl.valueChanges.subscribe(value => {
+      if (value) {
+        this.transformerSelected.emit(value);
+      }
+    });
+  }
 }
--- frontend/src/app/pages/transformers/transformers.component.ts
+++ frontend/src/app/pages/transformers/transformers.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { TransformerSelectorComponent } from '../../components/transformer-selector/transformer-selector.component';
 
 @Component({
   selector: 'app-transformers',
-  imports: [],
+  imports: [TransformerSelectorComponent],
   templateUrl: './transformers.component.html',
   styleUrl: './transformers.component.scss'
 })
 export class TransformersComponent {
 
+  onTransformerSelected(transformer: string): void {
+    console.log('Selected transformer:', transformer);
+  }
 }
