35bcb1b3b6aa6e7c5d6d2089f3b19d209b066a58
betinakd <AK221509@fi365.ort.edu.uy>
2025-06-07 07:40:40 -0300

    REFACTOR: Adapter transformers

--- backend/src/IAdapter/IExecutionAdapter.cs
+++ backend/src/IAdapter/IExecutionAdapter.cs
@@ -1,12 +1,10 @@
 using Models.Request;
 using Models.Response;
-using Transformers.Abstractions;
 
 namespace IAdapter;
 
 public interface IExecutionAdapter
 {
     MethodExecutionResponse ExecuteMethod(MethodExecutionRequest request);
-    TransformedResponse ExecuteMethodWithTransform(MethodExecutionRequest request, string transformerId);
     public bool IsAuthorizedUser(Guid apiKey);
 }
--- backend/src/IBusinessLogic/ITransformerService.cs
+++ backend/src/IBusinessLogic/ITransformerService.cs
@@ -1,9 +1,7 @@
-using Transformers.Abstractions;
-
 namespace IBusinessLogic;
+
 public interface ITransformerService
 {
-    void LoadTransformers();
-    IEnumerable<TransformerInfo> GetAvailableTransformers();
-    TransformedResponse TransformExecution(string executionResult, string transformerId);
+    string[] GetAvailableExporters();
+    public string ExportExecution(string exportType, string executionResult);
 }
