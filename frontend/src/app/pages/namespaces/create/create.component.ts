8e1b81f64daa1ec818ecd708c58d9e4a7b064dc3
betinakd <AK221509@fi365.ort.edu.uy>
2025-06-06 22:02:26 -0300

    add: create namespace page

--- frontend/src/app/pages/namespaces/create/create.component.ts
+++ frontend/src/app/pages/namespaces/create/create.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NamespaceService } from '../../../services/namespace.service';
+import { NamespaceSelectorComponent } from '../../../components/namespace-selector/namespace-selector.component';
+import { NamespaceRequest } from '../../../models/request/NamespaceRequest.model';
+import { CreatedNamespaceResponse } from '../../../models/CreatedNamespaceResponse';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, NamespaceSelectorComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit {
+  loading = false;
+  error = '';
+  name = '';
+  baseNamespaceId = '';
+  successMessage = '';
+  createdNamespace: CreatedNamespaceResponse | null = null;
 
+  constructor(private namespaceService: NamespaceService) { }
+
+  ngOnInit(): void {
+  }
+
+  onNamespaceSelected(value: string): void {
+    this.baseNamespaceId = value;
+  }
+
+  handleButtonClick() {
+    const namespaceData: NamespaceRequest = {
+      name: this.name,
+      baseNamespaceId: this.baseNamespaceId || null
+    };
+
+    this.successMessage = '';
+
+    this.namespaceService.createNamespace(namespaceData).subscribe({
+      next: (response) => {
+        this.successMessage = response.message + ' - ID: ' + response.namespaceResponse.id + ' - Name: ' + response.namespaceResponse.name;
+        this.loading = true;
+        this.createdNamespace = response;
+        this.error = '';
+        this.name = '';
+      },
+      error: (err) => {
+        this.error = 'Error: ' + (err.error?.message || err.message || 'Unknown error');
+        this.loading = false;
+        this.successMessage = '';
+      }
+    });
+  }
 }
