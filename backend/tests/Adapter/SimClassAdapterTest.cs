5545ccb36df680c3bfbe050ee267bceb339a10b4
max giménez <maxi_nos_10@hotmail.com>
2025-06-13 16:32:23 -0300

    REFACTOR: ImplementInterface Exceptions

--- backend/tests/Adapter/SimClassAdapterTest.cs
+++ backend/tests/Adapter/SimClassAdapterTest.cs
@@ -2,6 +2,7 @@ using Adapter;
 using BusinessLogic.Exceptions;
 using Domain;
 using Domain.Enums;
+using Domain.Exceptions;
 using IAdapter.Exceptions;
 using IBusinessLogic;
 using Models.Enums;
@@ -828,5 +829,254 @@ public class SimClassAdapterTest
         _mockSimClassService.Verify(s => s.GetSimClassById(returnTypeId), Times.AtLeastOnce);
         _mockSimClassService.Verify(s => s.ImplementInterface(It.IsAny<SimClass>()), Times.Once);
         _mockMethodService.Verify(m => m.IsValidVirtualOverride(It.IsAny<SimClass>(), It.IsAny<SimMethod>()), Times.AtLeastOnce);
+    }
+
+    [TestMethod]
+    public void ImplementInterface_ShouldThrowNonExistentValueAdapter_WhenInterfaceDoesNotExist()
+    {
+        var classId = Guid.NewGuid();
+        var interfaceId = Guid.NewGuid();
+
+        var implementRequest = new ImplementRequest
+        {
+            IdInterface = interfaceId.ToString(),
+            Methods = []
+        };
+
+        _mockSimClassService!
+            .Setup(s => s.GetSimClassById(interfaceId))
+            .Throws(new NonExistentValueLogic("Interface not found"));
+
+        var exception = Assert.ThrowsException<NonExistentValueAdapter>(() =>
+            _simClassAdapter!.ImplementInterface(classId, implementRequest));
+
+        Assert.AreEqual("Interface not found", exception.Message);
+        _mockSimClassService.Verify(s => s.GetSimClassById(interfaceId), Times.Once);
+    }
+
+    [TestMethod]
+    public void ImplementInterface_ShouldThrowNonExistentValueAdapter_WhenClassDoesNotExist()
+    {
+        var classId = Guid.NewGuid();
+        var interfaceId = Guid.NewGuid();
+
+        var interfaceClass = new SimClass
+        {
+            Id = interfaceId,
+            Name = "IMyInterface",
+            State = SimAccesibility.Interface,
+            Methods = []
+        };
+
+        var implementRequest = new ImplementRequest
+        {
+            IdInterface = interfaceId.ToString(),
+            Methods = []
+        };
+
+        _mockSimClassService!
+            .Setup(s => s.GetSimClassById(interfaceId))
+            .Returns(interfaceClass);
+
+        _mockSimClassService
+            .Setup(s => s.SimClassImplementsInterface(classId, interfaceId))
+            .Returns(false);
+
+        _mockSimClassService
+            .Setup(s => s.GetSimClassById(classId))
+            .Throws(new NonExistentValueLogic("Class not found"));
+
+        var exception = Assert.ThrowsException<NonExistentValueAdapter>(() =>
+            _simClassAdapter!.ImplementInterface(classId, implementRequest));
+
+        Assert.AreEqual("Class not found", exception.Message);
+        _mockSimClassService.Verify(s => s.GetSimClassById(classId), Times.Once);
+    }
+
+    [TestMethod]
+    public void ImplementInterface_ShouldThrowInvalidAttributeAdapter_WhenInvalidAttributeLogic()
+    {
+        var classId = Guid.NewGuid();
+        var interfaceId = Guid.NewGuid();
+
+        var interfaceClass = new SimClass
+        {
+            Id = interfaceId,
+            Name = "IMyInterface",
+            State = SimAccesibility.Interface,
+            Methods = []
+        };
+
+        var simClass = new SimClass
+        {
+            Id = classId,
+            Name = "MyClass",
+            State = SimAccesibility.Normal,
+            Implements = [],
+            Methods = []
+        };
+
+        var implementRequest = new ImplementRequest
+        {
+            IdInterface = interfaceId.ToString(),
+            Methods = []
+        };
+
+        _mockSimClassService!
+            .Setup(s => s.GetSimClassById(interfaceId))
+            .Returns(interfaceClass);
+
+        _mockSimClassService
+            .Setup(s => s.SimClassImplementsInterface(classId, interfaceId))
+            .Returns(false);
+
+        _mockSimClassService
+            .Setup(s => s.GetSimClassById(classId))
+            .Returns(simClass);
+
+        _mockSimClassService
+            .Setup(s => s.ImplementInterface(It.IsAny<SimClass>()))
+            .Throws(new InvalidAttributeLogic("Invalid method implementation"));
+
+        var exception = Assert.ThrowsException<InvalidAttributeAdapter>(() =>
+            _simClassAdapter!.ImplementInterface(classId, implementRequest));
+
+        Assert.AreEqual("Invalid method implementation", exception.Message);
+    }
+
+    [TestMethod]
+    public void ImplementInterface_ShouldThrowInUseValueAdapter_WhenClassIsInUse()
+    {
+        var classId = Guid.NewGuid();
+        var interfaceId = Guid.NewGuid();
+
+        var interfaceClass = new SimClass
+        {
+            Id = interfaceId,
+            Name = "IMyInterface",
+            State = SimAccesibility.Interface,
+            Methods = []
+        };
+
+        var simClass = new SimClass
+        {
+            Id = classId,
+            Name = "MyClass",
+            State = SimAccesibility.Normal,
+            Implements = [],
+            Methods = []
+        };
+
+        var implementRequest = new ImplementRequest
+        {
+            IdInterface = interfaceId.ToString(),
+            Methods = []
+        };
+
+        _mockSimClassService!
+            .Setup(s => s.GetSimClassById(interfaceId))
+            .Returns(interfaceClass);
+
+        _mockSimClassService
+            .Setup(s => s.SimClassImplementsInterface(classId, interfaceId))
+            .Returns(false);
+
+        _mockSimClassService
+            .Setup(s => s.GetSimClassById(classId))
+            .Returns(simClass);
+
+        _mockSimClassService
+            .Setup(s => s.ImplementInterface(It.IsAny<SimClass>()))
+            .Throws(new InUseValueLogic("Class is currently in use"));
+
+        var exception = Assert.ThrowsException<InUseValueAdapter>(() =>
+            _simClassAdapter!.ImplementInterface(classId, implementRequest));
+
+        Assert.AreEqual("Class is currently in use", exception.Message);
+    }
+
+    [TestMethod]
+    public void ImplementInterface_ShouldThrowInvalidAttributeAdapter_WhenInvalidAttributeDomain_OccursInParameter()
+    {
+        var classId = Guid.NewGuid();
+        var interfaceId = Guid.NewGuid();
+        var returnTypeId = Guid.NewGuid();
+
+        var interfaceClass = new SimClass
+        {
+            Id = interfaceId,
+            Name = "IMyInterface",
+            State = SimAccesibility.Interface,
+            Methods = []
+        };
+
+        var simClass = new SimClass
+        {
+            Id = classId,
+            Name = "MyClass",
+            State = SimAccesibility.Normal,
+            Implements = [],
+            Methods = []
+        };
+
+        var returnType = new SimClass
+        {
+            Id = returnTypeId,
+            Name = "ReturnType"
+        };
+
+        var methodRequest = new MethodRequest
+        {
+            Name = "TestMethod",
+            Accesibility = SimModelsAccesibility.Normal,
+            Privacity = SimModelsPrivacity.Public,
+            IdReturnType = returnTypeId.ToString(),
+            IsStatic = false,
+            IsVirtual = false,
+            IsOverride = false,
+            Parameters =
+            [
+                new ParameterRequest
+            {
+                Name = "param1",
+                IdReference = returnTypeId.ToString()
+            }
+
+            ]
+        };
+
+        var implementRequest = new ImplementRequest
+        {
+            IdInterface = interfaceId.ToString(),
+            Methods = [methodRequest]
+        };
+
+        _mockSimClassService!
+            .Setup(s => s.GetSimClassById(interfaceId))
+            .Returns(interfaceClass);
+
+        _mockSimClassService
+            .Setup(s => s.SimClassImplementsInterface(classId, interfaceId))
+            .Returns(false);
+
+        _mockSimClassService
+            .Setup(s => s.GetSimClassById(classId))
+            .Returns(simClass);
+
+        _mockSimClassService
+            .Setup(s => s.GetSimClassById(returnTypeId))
+            .Throws(new InvalidAttributeDomain("Invalid parameter type."));
+
+        _mockSimClassService
+            .Setup(s => s.ImplementInterface(It.IsAny<SimClass>()))
+            .Returns((SimClass c) => c);
+
+        _mockMethodService!
+            .Setup(m => m.IsValidVirtualOverride(It.IsAny<SimClass>(), It.IsAny<SimMethod>()));
+
+        var exception = Assert.ThrowsException<InvalidAttributeAdapter>(() =>
+            _simClassAdapter!.ImplementInterface(classId, implementRequest));
+
+        Assert.AreEqual("Invalid parameter type.", exception.Message);
     }
 }
